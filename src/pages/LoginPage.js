2d7e8c547ca43cbd0b248dbcf15611dd89b0d9e0
Vanessa Tan <vanessatjx25@gmail.com>
2025-05-30 15:14:45 +0800

    bare login page

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,22 @@
+// src/pages/LoginPage.js
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex">
+      {/* Left Side */}
+      <div className="w-1/2 bg-gray-100 hidden md:block">
+        {/* Add your logo or image here */}
+        <div className="h-full flex items-center justify-center">
+          <h1 className="text-4xl font-bold text-gray-400">Afterlife</h1>
+        </div>
+      </div>
+
+      {/* Right Side */}
+      <div className="w-full md:w-1/2 flex items-center justify-center p-8">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
