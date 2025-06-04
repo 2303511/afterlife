59ffa1d068d1f3f74f15792ee367a39e43a05ca4
kxnjo <84301894+kxnjo@users.noreply.github.com>
2025-06-04 15:31:07 +0800

    update sql scripts

--- create_table_script.sql
+++ create_table_script.sql
@@ -2,64 +2,60 @@ CREATE SCHEMA `AfterLifeDB`;
 
 USE AfterLifeDB;
 
--- role table
+-- Role table
 CREATE TABLE Role (
-    roleId SERIAL PRIMARY KEY,
+    roleId CHAR(36) PRIMARY KEY,
     roleName VARCHAR(255)
 );
 
--- user table
-CREATE TABLE
-    User (
-        userId CHAR(36) PRIMARY KEY,
-        username VARCHAR(255),
-        hashedPassword VARCHAR(255),
-        fullName VARCHAR(255),
-        contactNumber VARCHAR(20),
-        nric VARCHAR(20),
-        dob DATE,
-        nationality VARCHAR(255),
-        userAddress TEXT,
-        appliedUrns TEXT,
-        roleId BIGINT UNSIGNED,
-        FOREIGN KEY (roleId) REFERENCES Role (roleId)
-    );
+-- User table
+CREATE TABLE User (
+    userId CHAR(36) PRIMARY KEY,
+    username VARCHAR(255),
+    hashedPassword VARCHAR(255),
+    fullName VARCHAR(255),
+    contactNumber VARCHAR(20),
+    nric VARCHAR(20),
+    dob DATE,
+    nationality VARCHAR(255),
+    userAddress TEXT,
+    appliedUrns TEXT,
+    roleId CHAR(36),
+    FOREIGN KEY (roleId) REFERENCES Role(roleId)
+);
 
--- building table
-CREATE TABLE
-    Building (
-        buildingID SERIAL PRIMARY KEY,
-        buildingName VARCHAR(255),
-        buildingLocation TEXT
-    );
+-- Building table
+CREATE TABLE Building (
+    buildingID CHAR(36) PRIMARY KEY,
+    buildingName VARCHAR(255),
+    buildingLocation TEXT
+);
 
--- level table
-CREATE TABLE
-    Level (
-        levelID SERIAL PRIMARY KEY,
-        buildingID BIGINT UNSIGNED,
-        levelNumber INT,
-        notes TEXT,
-        FOREIGN KEY (buildingID) REFERENCES Building (buildingID)
-    );
+-- Level table
+CREATE TABLE Level (
+    levelID CHAR(36) PRIMARY KEY,
+    buildingID CHAR(36),
+    levelNumber INT,
+    notes TEXT,
+    FOREIGN KEY (buildingID) REFERENCES Building(buildingID)
+);
 
--- block table
-CREATE TABLE
-    Block (
-        blockID SERIAL PRIMARY KEY,
-        levelID BIGINT UNSIGNED,
-        levelNumber INT,
-        notes TEXT,
-        FOREIGN KEY (levelID) REFERENCES Level (levelID)
-    );
+-- Block table
+CREATE TABLE Block (
+    blockID CHAR(36) PRIMARY KEY,
+    levelID CHAR(36),
+    levelNumber INT,
+    notes TEXT,
+    FOREIGN KEY (levelID) REFERENCES Level(levelID)
+);
 
--- niche table
+-- Niche table
 CREATE TABLE Niche (
-    nicheID SERIAL PRIMARY KEY,
-    blockID BIGINT UNSIGNED, -- [FK] blockID from Block table
+    nicheID CHAR(36) PRIMARY KEY,
+    blockID CHAR(36),
     nicheColumn INT,
     nicheRow INT,
-    niche_code VARCHAR(50), -- indicates niche column, and row. ie. 01-07 = column 1, row 7 (IS THIS REDUNDANT??)
+    niche_code VARCHAR(50),
     status ENUM('Available', 'Pending', 'Reserved', 'Occupied'),
     lastUpdated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     dateOfBirth DATE,
@@ -67,26 +63,35 @@ CREATE TABLE Niche (
     birthCertificate TEXT,
     deathCertificate TEXT,
     occupantName VARCHAR(50),
-    urnID BIGINT UNSIGNED, -- [FK] urnID from Urn table
-    bookingID CHAR(36), -- [FK] bookingID from Booking table
+    bookingID CHAR(36),
     FOREIGN KEY (blockID) REFERENCES Block(blockID),
-    FOREIGN KEY (urnID) REFERENCES Urn(urnID),
     FOREIGN KEY (bookingID) REFERENCES Booking(bookingID)
 );
 
--- booking table
+-- Urn table
+CREATE TABLE Urn (
+    urnID CHAR(36) PRIMARY KEY,
+    inscription TEXT,
+    inserted_at TIMESTAMP,
+    applicantID CHAR(36),
+    nicheID CHAR(36),
+    FOREIGN KEY (applicantID) REFERENCES User(userId),
+    FOREIGN KEY (nicheID) REFERENCES Niche(nicheID)
+);
+
+-- Booking table
 CREATE TABLE Booking (
     bookingID CHAR(36) PRIMARY KEY,
-    nicheID BIGINT UNSIGNED,
-    paidByID BIGINT UNSIGNED,
-    paymentID BIGINT UNSIGNED,
+    nicheID CHAR(36),
+    paidByID CHAR(36),
+    paymentID CHAR(36),
     bookingType ENUM('Current', 'PreOrder'),
     FOREIGN KEY (nicheID) REFERENCES Niche(nicheID),
     FOREIGN KEY (paidByID) REFERENCES User(userId),
     FOREIGN KEY (paymentID) REFERENCES Payment(paymentID)
 );
 
--- payment table
+-- Payment table
 CREATE TABLE Payment (
     paymentID CHAR(36) PRIMARY KEY,
     bookingID CHAR(36),
@@ -95,15 +100,4 @@ CREATE TABLE Payment (
     paymentDate DATE,
     paymentStatus ENUM('Pending', 'Cancelled', 'Refunded', 'Fully Paid'),
     FOREIGN KEY (bookingID) REFERENCES Booking(bookingID)
-);
-
--- urn table
-CREATE TABLE Urn (
-    urnID SERIAL PRIMARY KEY,
-    inscription TEXT,
-    inserted_at TIMESTAMP,
-    applicantID BIGINT UNSIGNED,
-    nicheID BIGINT UNSIGNED,
-    FOREIGN KEY (applicantID) REFERENCES User(userId),
-    FOREIGN KEY (nicheID) REFERENCES Niche(nicheID)
 );
--- dummy_data_inserts.sql
+++ dummy_data_inserts.sql
@@ -1,55 +1,36 @@
--- INSERT DUMMY DATA SCRIPTS
+USE AfterLifeDB;
 
--- insert dummy data into Role table
-INSERT INTO Role (roleId, roleName) VALUES (1, 'Admin');
-INSERT INTO Role (roleId, roleName) VALUES (2, 'Staff');
-INSERT INTO Role (roleId, roleName) VALUES (3, 'Applicant');
+INSERT INTO Role (roleId, roleName) VALUES ('fc3ee35b-af2f-4c8f-9a51-93984ee66ce1', 'Admin');
+INSERT INTO Role (roleId, roleName) VALUES ('8a3003ac-af4b-4661-b96f-f5cb208c05f1', 'User');
+INSERT INTO Role (roleId, roleName) VALUES ('9a6b5b55-38d8-45e2-9e1b-a280b9dee533', 'Staff');
 
--- insert dummy data into Building table
-INSERT INTO Building (buildingID, buildingName, buildingLocation) VALUES (1, 'Columbarium A', 'Downtown');
-INSERT INTO Building (buildingID, buildingName, buildingLocation) VALUES (2, 'Columbarium B', 'Uptown');
+INSERT INTO User (userId, username, hashedPassword, fullName, contactNumber, nric, dob, nationality, userAddress, appliedUrns, roleId) VALUES ('ee0df131-9b7e-4f72-a735-df613dad8544', 'user0', 'hashed_password_0', 'User Fullname 0', '91234560', 'S1234560A', '1980-01-01', 'Singaporean', 'Blk 10 Clementi Ave', '', 'fc3ee35b-af2f-4c8f-9a51-93984ee66ce1');
+INSERT INTO User (userId, username, hashedPassword, fullName, contactNumber, nric, dob, nationality, userAddress, appliedUrns, roleId) VALUES ('04a49a5c-018f-48f4-b376-e2eb9ae6f7b6', 'user1', 'hashed_password_1', 'User Fullname 1', '91234561', 'S1234561A', '1981-01-01', 'Singaporean', 'Blk 11 Clementi Ave', '', '8a3003ac-af4b-4661-b96f-f5cb208c05f1');
+INSERT INTO User (userId, username, hashedPassword, fullName, contactNumber, nric, dob, nationality, userAddress, appliedUrns, roleId) VALUES ('dcd5af57-e5a9-4731-b7bd-26d231275c77', 'user2', 'hashed_password_2', 'User Fullname 2', '91234562', 'S1234562A', '1982-01-01', 'Singaporean', 'Blk 12 Clementi Ave', '', '8a3003ac-af4b-4661-b96f-f5cb208c05f1');
 
--- insert dummy data into Level table
-INSERT INTO Level (levelID, buildingID, levelNumber, notes) VALUES (1, 1, 1, 'Ground Floor');
-INSERT INTO Level (levelID, buildingID, levelNumber, notes) VALUES (2, 1, 2, 'Second Floor');
-INSERT INTO Level (levelID, buildingID, levelNumber, notes) VALUES (3, 2, 1, 'Ground Floor');
+INSERT INTO Building (buildingID, buildingName, buildingLocation) VALUES ('d6690e03-aaea-4b9c-9e87-dee1ea210db7', 'Columbarium Tower 1', 'Location 1');
+INSERT INTO Building (buildingID, buildingName, buildingLocation) VALUES ('82d741c8-6697-4306-89d1-83d36f978ed8', 'Columbarium Tower 2', 'Location 2');
 
--- insert dummy data into Block table
-INSERT INTO Block (blockID, levelID, levelNumber, notes) VALUES (1, 1, 1, 'Left Wing');
-INSERT INTO Block (blockID, levelID, levelNumber, notes) VALUES (2, 1, 2, 'Right Wing');
-INSERT INTO Block (blockID, levelID, levelNumber, notes) VALUES (3, 2, 1, 'Center Wing');
+INSERT INTO Level (levelID, buildingID, levelNumber, notes) VALUES ('36b7d8f4-9f8c-4759-b73a-1139920c01dd', 'd6690e03-aaea-4b9c-9e87-dee1ea210db7', '1', 'Level 1 notes');
+INSERT INTO Level (levelID, buildingID, levelNumber, notes) VALUES ('8fcf0b8e-eb6e-4a4b-8cf9-4bfa2541399a', '82d741c8-6697-4306-89d1-83d36f978ed8', '2', 'Level 2 notes');
+INSERT INTO Level (levelID, buildingID, levelNumber, notes) VALUES ('1a69fa0d-e308-4bba-9b44-2d73157e6ee3', 'd6690e03-aaea-4b9c-9e87-dee1ea210db7', '3', 'Level 3 notes');
 
--- insert dummy data into User table
-INSERT INTO User (userId, username, hashedPassword, fullName, contactNumber, nric, dob, nationality, userAddress, appliedUrns, roleId) VALUES ('95f64ad5-ab9e-4558-9487-330231c58856', 'user0', 'hashed_pw_0', 'User Fullname 0', '91234560', 'S1234560A', '1986-10-16', 'Singaporean', 'Address 0', '', 1);
-INSERT INTO User (userId, username, hashedPassword, fullName, contactNumber, nric, dob, nationality, userAddress, appliedUrns, roleId) VALUES ('9547c65f-1f65-422e-ad1b-4dd3dc059217', 'user1', 'hashed_pw_1', 'User Fullname 1', '91234561', 'S1234561A', '1970-10-15', 'Singaporean', 'Address 1', '', 2);
-INSERT INTO User (userId, username, hashedPassword, fullName, contactNumber, nric, dob, nationality, userAddress, appliedUrns, roleId) VALUES ('dbba3960-1339-4123-9235-b4606838d7d6', 'user2', 'hashed_pw_2', 'User Fullname 2', '91234562', 'S1234562A', '1978-12-17', 'Singaporean', 'Address 2', '', 2);
-INSERT INTO User (userId, username, hashedPassword, fullName, contactNumber, nric, dob, nationality, userAddress, appliedUrns, roleId) VALUES ('8b7c47b7-d94f-40f8-988d-5de16b8903bf', 'user3', 'hashed_pw_3', 'User Fullname 3', '91234563', 'S1234563A', '1997-05-14', 'Singaporean', 'Address 3', '', 2);
-INSERT INTO User (userId, username, hashedPassword, fullName, contactNumber, nric, dob, nationality, userAddress, appliedUrns, roleId) VALUES ('fb02b35b-11db-49db-9cc7-56a824ddd4fc', 'user4', 'hashed_pw_4', 'User Fullname 4', '91234564', 'S1234564A', '1988-04-21', 'Singaporean', 'Address 4', '', 3);
+INSERT INTO Block (blockID, levelID, levelNumber, notes) VALUES ('4eddf318-3ca6-4df6-a669-3e90807fae11', '36b7d8f4-9f8c-4759-b73a-1139920c01dd', '1', 'Block 1 notes');
+INSERT INTO Block (blockID, levelID, levelNumber, notes) VALUES ('2900250e-f165-4db2-8ec5-73a0bf344423', '8fcf0b8e-eb6e-4a4b-8cf9-4bfa2541399a', '2', 'Block 2 notes');
+INSERT INTO Block (blockID, levelID, levelNumber, notes) VALUES ('3190ff47-bb46-4201-9b4e-6b4147c0920b', '1a69fa0d-e308-4bba-9b44-2d73157e6ee3', '3', 'Block 3 notes');
 
--- insert dummy data into Urn table
-INSERT INTO Urn (urnID, inscription, inserted_at, applicantID, nicheID) VALUES (1, 'In loving memory of Person 0', '2025-06-02 14:08:05', '95f64ad5-ab9e-4558-9487-330231c58856', 1);
-INSERT INTO Urn (urnID, inscription, inserted_at, applicantID, nicheID) VALUES (2, 'In loving memory of Person 1', '2025-06-02 14:08:05', '9547c65f-1f65-422e-ad1b-4dd3dc059217', 2);
-INSERT INTO Urn (urnID, inscription, inserted_at, applicantID, nicheID) VALUES (3, 'In loving memory of Person 2', '2025-06-02 14:08:05', 'dbba3960-1339-4123-9235-b4606838d7d6', 3);
-INSERT INTO Urn (urnID, inscription, inserted_at, applicantID, nicheID) VALUES (4, 'In loving memory of Person 3', '2025-06-02 14:08:05', '8b7c47b7-d94f-40f8-988d-5de16b8903bf', 4);
-INSERT INTO Urn (urnID, inscription, inserted_at, applicantID, nicheID) VALUES (5, 'In loving memory of Person 4', '2025-06-02 14:08:05', 'fb02b35b-11db-49db-9cc7-56a824ddd4fc', 5);
+INSERT INTO Niche (nicheID, blockID, nicheColumn, nicheRow, niche_code, status, dateOfBirth, dateOfDeath, birthCertificate, deathCertificate, occupantName, bookingID) VALUES ('7897d582-8cd9-4da2-8334-7a9cc8fb6080', '4eddf318-3ca6-4df6-a669-3e90807fae11', '1', '2', '01-02', 'Pending', '1950-05-01', '2000-05-01', 'BC0001', 'DC0001', 'Occupant 1', '9dca1ade-5684-4282-8fdf-0a4bfd71e729');
+INSERT INTO Niche (nicheID, blockID, nicheColumn, nicheRow, niche_code, status, dateOfBirth, dateOfDeath, birthCertificate, deathCertificate, occupantName, bookingID) VALUES ('e9307336-1495-48e7-9dbf-9d17255420e2', '2900250e-f165-4db2-8ec5-73a0bf344423', '2', '3', '02-03', 'Reserved', '1951-05-01', '2001-05-01', 'BC0002', 'DC0002', 'Occupant 2', '9f968b7c-4ae6-4dc9-b6a4-71de416f1dcc');
+INSERT INTO Niche (nicheID, blockID, nicheColumn, nicheRow, niche_code, status, dateOfBirth, dateOfDeath, birthCertificate, deathCertificate, occupantName, bookingID) VALUES ('0da1de34-e927-4864-a05a-266199be69f5', '3190ff47-bb46-4201-9b4e-6b4147c0920b', '3', '4', '03-04', 'Pending', '1952-05-01', '2002-05-01', 'BC0003', 'DC0003', 'Occupant 3', '3aaa9459-ca3d-43e8-b4a9-3bd1b6c49783');
 
--- insert dummy data into Niche table
-INSERT INTO Niche (nicheID, blockID, nicheColumn, nicheRow, niche_code, status, lastUpdated, dateOfBirth, dateOfDeath, birthCertificate, deathCertificate, occupantName, urnID, bookingID) VALUES (1, 1, 3, 8, '03-08', 'Available', '2025-06-02 14:08:05', NULL, NULL, '', '', 'Person 0', 1, 1);
-INSERT INTO Niche (nicheID, blockID, nicheColumn, nicheRow, niche_code, status, lastUpdated, dateOfBirth, dateOfDeath, birthCertificate, deathCertificate, occupantName, urnID, bookingID) VALUES (2, 1, 1, 2, '01-02', 'Available', '2025-06-02 14:08:05', NULL, NULL, '', '', 'Person 1', 2, 2);
-INSERT INTO Niche (nicheID, blockID, nicheColumn, nicheRow, niche_code, status, lastUpdated, dateOfBirth, dateOfDeath, birthCertificate, deathCertificate, occupantName, urnID, bookingID) VALUES (3, 1, 1, 9, '01-09', 'Available', '2025-06-02 14:08:05', NULL, NULL, '', '', 'Person 2', 3, 3);
-INSERT INTO Niche (nicheID, blockID, nicheColumn, nicheRow, niche_code, status, lastUpdated, dateOfBirth, dateOfDeath, birthCertificate, deathCertificate, occupantName, urnID, bookingID) VALUES (4, 3, 3, 2, '03-02', 'Reserved', '2025-06-02 14:08:05', NULL, NULL, '', '', 'Person 3', 4, 4);
-INSERT INTO Niche (nicheID, blockID, nicheColumn, nicheRow, niche_code, status, lastUpdated, dateOfBirth, dateOfDeath, birthCertificate, deathCertificate, occupantName, urnID, bookingID) VALUES (5, 2, 4, 1, '04-01', 'Reserved', '2025-06-02 14:08:05', NULL, NULL, '', '', 'Person 4', 5, 5);
+INSERT INTO Urn (urnID, inscription, inserted_at, applicantID, nicheID) VALUES ('ea3ed0a3-a15b-4ad4-8c90-2f113d1d643f', 'In Loving Memory of Occupant 1', '2025-06-04', 'ee0df131-9b7e-4f72-a735-df613dad8544', '7897d582-8cd9-4da2-8334-7a9cc8fb6080');
+INSERT INTO Urn (urnID, inscription, inserted_at, applicantID, nicheID) VALUES ('0e577a49-f72b-461e-b9a9-25ba06dd6cc5', 'In Loving Memory of Occupant 2', '2025-06-04', '04a49a5c-018f-48f4-b376-e2eb9ae6f7b6', 'e9307336-1495-48e7-9dbf-9d17255420e2');
+INSERT INTO Urn (urnID, inscription, inserted_at, applicantID, nicheID) VALUES ('13960b8d-6a93-4ae6-b206-5e784a440e7d', 'In Loving Memory of Occupant 3', '2025-06-04', 'dcd5af57-e5a9-4731-b7bd-26d231275c77', '0da1de34-e927-4864-a05a-266199be69f5');
 
--- insert dummy data into Booking table
-INSERT INTO Booking (bookingID, nicheID, paidByID, paymentID, bookingType) VALUES (1, 1, '95f64ad5-ab9e-4558-9487-330231c58856', NULL, 'Current');
-INSERT INTO Booking (bookingID, nicheID, paidByID, paymentID, bookingType) VALUES (2, 2, '9547c65f-1f65-422e-ad1b-4dd3dc059217', NULL, 'Current');
-INSERT INTO Booking (bookingID, nicheID, paidByID, paymentID, bookingType) VALUES (3, 3, 'dbba3960-1339-4123-9235-b4606838d7d6', NULL, 'PreOrder');
-INSERT INTO Booking (bookingID, nicheID, paidByID, paymentID, bookingType) VALUES (4, 4, '8b7c47b7-d94f-40f8-988d-5de16b8903bf', NULL, 'PreOrder');
-INSERT INTO Booking (bookingID, nicheID, paidByID, paymentID, bookingType) VALUES (5, 5, 'fb02b35b-11db-49db-9cc7-56a824ddd4fc', NULL, 'PreOrder');
+INSERT INTO Payment (paymentID, bookingID, amount, paymentMethod, paymentDate, paymentStatus) VALUES ('7e6494d2-7008-4edd-a015-b00838969b1d', '9dca1ade-5684-4282-8fdf-0a4bfd71e729', '500.0', 'Credit Card', '2025-06-04', 'Pending');
+INSERT INTO Payment (paymentID, bookingID, amount, paymentMethod, paymentDate, paymentStatus) VALUES ('d9152ebc-3210-451d-942b-92faef07d4c1', '9f968b7c-4ae6-4dc9-b6a4-71de416f1dcc', '600.0', 'Credit Card', '2025-05-25', 'Cancelled');
+INSERT INTO Payment (paymentID, bookingID, amount, paymentMethod, paymentDate, paymentStatus) VALUES ('ba96cd40-0468-442b-8939-9bb2f74e5018', '3aaa9459-ca3d-43e8-b4a9-3bd1b6c49783', '700.0', 'Credit Card', '2025-05-15', 'Fully Paid');
 
--- insert dummy data into Payment table
-INSERT INTO Payment (paymentID, bookingID, amount, paymentMethod, paymentDate, paymentStatus) VALUES (1, 1, 3391.23, 'Credit Card', '2025-06-02', 'Pending');
-INSERT INTO Payment (paymentID, bookingID, amount, paymentMethod, paymentDate, paymentStatus) VALUES (2, 2, 2785.47, 'Credit Card', '2025-06-02', 'Fully Paid');
-INSERT INTO Payment (paymentID, bookingID, amount, paymentMethod, paymentDate, paymentStatus) VALUES (3, 3, 4563.03, 'Credit Card', '2025-06-02', 'Fully Paid');
-INSERT INTO Payment (paymentID, bookingID, amount, paymentMethod, paymentDate, paymentStatus) VALUES (4, 4, 3855.16, 'Credit Card', '2025-06-02', 'Fully Paid');
-INSERT INTO Payment (paymentID, bookingID, amount, paymentMethod, paymentDate, paymentStatus) VALUES (5, 5, 4191.68, 'Credit Card', '2025-06-02', 'Pending');
+INSERT INTO Booking (bookingID, nicheID, paidByID, paymentID, bookingType) VALUES ('9dca1ade-5684-4282-8fdf-0a4bfd71e729', '7897d582-8cd9-4da2-8334-7a9cc8fb6080', 'ee0df131-9b7e-4f72-a735-df613dad8544', '7e6494d2-7008-4edd-a015-b00838969b1d', 'PreOrder');
+INSERT INTO Booking (bookingID, nicheID, paidByID, paymentID, bookingType) VALUES ('9f968b7c-4ae6-4dc9-b6a4-71de416f1dcc', 'e9307336-1495-48e7-9dbf-9d17255420e2', '04a49a5c-018f-48f4-b376-e2eb9ae6f7b6', 'd9152ebc-3210-451d-942b-92faef07d4c1', 'PreOrder');
+INSERT INTO Booking (bookingID, nicheID, paidByID, paymentID, bookingType) VALUES ('3aaa9459-ca3d-43e8-b4a9-3bd1b6c49783', '0da1de34-e927-4864-a05a-266199be69f5', 'dcd5af57-e5a9-4731-b7bd-26d231275c77', 'ba96cd40-0468-442b-8939-9bb2f74e5018', 'Current');
