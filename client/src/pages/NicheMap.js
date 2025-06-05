6f831c81d2b947906e5305e505c311a017ae7130
Vanessa Tan <vanessatjx25@gmail.com>
2025-06-05 12:46:45 +0800

    updated css import

--- client/src/pages/NicheMap.js
+++ client/src/pages/NicheMap.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../components/NicheGridSelector.css";
+import "../styles/Niche-Management.css";
 
 import LocationSelector from "../components/LocationSelector";
 import NicheLegend from "../components/NicheLegend";
